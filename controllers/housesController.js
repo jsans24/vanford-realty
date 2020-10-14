41e07f10c3403fe7a5b0eacdbd04096df65f522d
williamv1 <wmvanhook@gmail.com>
2020-10-14 12:47:40 -0400

    Add info to show houses

--- controllers/housesController.js
+++ controllers/housesController.js
@@ -43,14 +43,14 @@ router.get('/search', (req, res) => {
 router.get('/:id', (req, res) => {
     db.House.findById(req.params.id, (err, listing) => {
         if(err) return console.log(err);
-
-        db.Realtor.findById(req.params.realtor, (err, realtor) => {
+        console.log(listing);
+        db.Realtor.findById(listing.realtor, (err, realtor) => {
             if(err) return console.log(err);
 
             res.render('houses/show', {
                 listing: listing,
                 user: req.user,
-                realtor
+                realtor: realtor
             })
         })
 
